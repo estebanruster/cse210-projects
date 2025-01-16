e0a8082d1ccee6d3fbc0467db650016b1dd09bdb
estebanruster <emangascalva@byupathway.edu>
2025-01-15 18:18:42 -0600

    Exercise 3 solved. A random number is generated and the program keeps runing giving the user info until the magic number y guessed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        //Random class instance created
+        Random randomGenerator = new Random();
+        //Instance used to get the next integer between 1 to 100
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess;
+
+        Console.WriteLine("Guess the magic number between 1 to 100!");
+
+        do
+        {
+            Console.Write("What is your guess? (Type a number): ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed the magic number!");
+            }
+        } while (guess != magicNumber);
     }
 }
