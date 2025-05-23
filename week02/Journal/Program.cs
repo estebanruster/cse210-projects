c04c8332960d5372a1f72607afe67f5017699fdb
estebanruster <emangascalva@byupathway.edu>
2025-05-23 14:23:57 -0600

    Testing the PromptGenerator class and its method. The string is return correctly and is selected ranbomly from the list with the .Next method of a Random class

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -11,10 +11,30 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
 
+        //Instantiate PromptGenerator class to create a list with the prompts
+        PromptGenerator prompts = new PromptGenerator();
+        /*
+        string prompt1 = "What was the strongest emotion I felt today and why?";
+        prompts._prompts.Add(prompt1);
+        string prompt2 = "If I had one thing I could do over today, what would it be?";
+        prompts._prompts.Add(prompt2);
+        string prompt3 = "What is one thing I could learn from today?";
+        prompts._prompts.Add(prompt3);
+        string prompt4 = "Who was the most interesting person I interacted with today and why?";
+        prompts._prompts.Add(prompt4);
+        string prompt5 = "For what are you grateful today and why?";
+        prompts._prompts.Add(prompt5);
+        string prompt6 = "What was the funniest thing that happened today?";
+        prompts._prompts.Add(prompt6);
+        */
+
+        //Getting the random prompt
+
+
         //Testing Entry class member variables and method
         Entry entry = new Entry();
-        //Prompt place-holder. Later get it from PromptGenerator class
-        entry._prompt = "This is the prompt. Write something.";
+        //Getting the random prompt
+        entry._prompt = prompts.GetRandomPrompt();
         //Later, implemente DateTime to get the date from the computer
         entry._dateText = "5/21/2025";
         Console.WriteLine(entry._prompt);
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -12,7 +12,18 @@ public class PromptGenerator
     //because it is later used and saved in the Entry class.
     public string GetRandomPrompt()
     {
-
-        return "";
+        //Random class instance created
+        Random randomGenerator = new Random();
+        //Creating a list with the prompts
+        _prompts.Add("What was the strongest emotion I felt today and why?");
+        _prompts.Add("If I had one thing I could do over today, what would it be?");
+        _prompts.Add("What is one thing I could learn from today?");
+        _prompts.Add("Who was the most interesting person I interacted with today and why?");
+        _prompts.Add("For what are you grateful today and why?");
+        _prompts.Add("What was the funniest thing that happened today?");
+        //Instance used to get the next integer between 0 to 5 of the list index
+        int randomIndex = randomGenerator.Next(0, 6);
+        Console.WriteLine(randomIndex);
+        return _prompts[randomIndex];
     }
 }
